3eef9bf22b27f4bf10ba20095c45634b7a4c65c4
tristanvong <146710405+tristanvong@users.noreply.github.com>
2024-12-26 16:18:13 +0100

    Made usercontroller to delete and makeadmin
    When deleting an user their reviews and recipes get deleted.

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,18 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Models\Recipe;
+use App\Models\User;
+use App\Http\Controllers\UserController;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/test', function () {
+    $recipes = Recipe::with('user', 'reviews')->get();
+    $users = User::all();
+    return view('test', compact('recipes', 'users'));
 });
 
 Route::get('/dashboard', function () {
@@ -17,4 +26,6 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+Route::delete('/user/{id}', [UserController::class, 'destroy'])->name('user.destroy');
+Route::post('/make-admin/{id}', [UserController::class, 'makeAdmin'])->name('makeAdmin');//have to add middleware to make sure that only admin can do this
 require __DIR__.'/auth.php';
