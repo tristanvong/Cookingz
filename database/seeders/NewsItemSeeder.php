717b7f303c910af6ff6e02abfb8d0c7bb2132911
tristanvong <146710405+tristanvong@users.noreply.github.com>
2024-12-27 18:51:13 +0100

    Fixed image, "imageUrl() is deprecated
    A placeholder from wikimedia commons will be shown instead of the deprecated "imageUrl" that isnt supported anymore.

--- database/seeders/NewsItemSeeder.php
+++ database/seeders/NewsItemSeeder.php
@@ -20,7 +20,7 @@ class NewsItemSeeder extends Seeder
             NewsItem::create([
                 'title' => $faker->sentence(),
                 'content' => $faker->paragraph(),
-                'image' => $faker->imageUrl(),
+                'image' => rand(0, 1) ? "https://upload.wikimedia.org/wikipedia/commons/3/3f/Placeholder_view_vector.svg" : null,
                 'published_at' => $faker->dateTimeBetween('-1 year', 'now'),
             ]);
         }
